44cae842c3eea979d86921603ea67a17556ded00
hoangminhdu <hoangminhdu2000@gmail.com>
2020-12-19 00:59:55 +0700

    training page

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 from flask import Flask, render_template, request
 from scipy.io import arff
 from io import StringIO
-import main, preprocess
+import main, preprocess, process
 
 app=Flask(__name__)
 
@@ -16,36 +16,84 @@ def index():
         
 @app.route('/datainput', methods=['GET', 'POST'])
 def datainput():
-    arrfurl = "1year.arff"
-    rawDfList = main.getRawDfList(arrfurl);
-    rawDfList[0] = rawDfList[0][rawDfList[0]['Attr1'] > 0.3]  # less value for dataframe
-    Df_missing_stats = main.getMissingStats(rawDfList)
+    df, Df_missing_stats = preProcessData("1year", "null")
     return render_template("datainput.html", 
-        tables=[rawDfList[0].to_html(classes='table', header="true")], 
+        tables=[df.to_html(classes='table', header="true")], 
         missingstats = [Df_missing_stats.to_html(classes="table missing-stats", header="true")])
 
 
 @app.route('/choosedata/<string:chosendata>/<string:chosenimputation>', methods=['GET', 'POST'])
 def chooseData(chosendata, chosenimputation):
-    arrfurl = chosendata + ".arff"
-    rawDfList = main.getRawDfList(arrfurl);
-    if (chosenimputation != "null"):
-        rawDfList = preprocess.meanImputation(rawDfList)
-    rawDfList[0] = rawDfList[0][rawDfList[0]['Attr1'] > 0.3]  # less value for dataframe
-    Df_missing_stats = main.getMissingStats(rawDfList)
+    df, Df_missing_stats = preProcessData(chosendata, chosenimputation)
     return render_template("datainput.html", 
-        tables=[rawDfList[0].to_html(classes='table', header="true")], 
+        tables=[df.to_html(classes='table', header="true")], 
         missingstats = [Df_missing_stats.to_html(classes="table missing-stats", header="true")])
 
 
+#test
 @app.route('/datapreprocess', methods=['GET', 'POST'])
-def datapreprocess():
+def datapreprocesstest():
     return render_template("datapreprocess.html")
+
+
+@app.route('/choosedatapreprocess/<string:chosendata>/<string:chosenimputation>', methods=['GET', 'POST'])
+def datapreprocess(chosendata, chosenimputation):
+    return render_template("datapreprocess.html")
+
+
+@app.route('/choosedatatrain/<string:chosendata>/<string:chosenimputation>/<string:chosenmethod>/<string:chosentraintest>/<int:value>', methods=['GET', 'POST'])
+def choosedatatrain(chosendata, chosenimputation, chosenmethod, chosentraintest, value):
+    df, Df_missing_stats = preProcessData(chosendata, chosenimputation)
+    if (chosentraintest == "split"):
+        if (value != "null"):
+            X_train, X_test, y_train, y_test = process.traintestsplit(df, value)
+        else:
+            X_train, X_test, y_train, y_test = process.traintestsplit(df)
+
+    if (chosenmethod == "decisiontree"):
+        confusion_matrix, accuracy_score, classification_report = process.decisionTree(X_train, X_test, y_train, y_test)
+    print('confusion_matrix')
+    print(confusion_matrix)
+    print('accuracy_score')
+    print(accuracy_score)
+    print('classification_report')
+    
+    for key, values in classification_report.items():
+        print(key)
+        print('values')
+        values['f1score'] = values.pop('f1-score')
+
+        
+        
+
+    print(classification_report)
+
+    return render_template("datapreprocess.html", 
+        confusion_matrix = confusion_matrix, 
+        accuracy_score = accuracy_score, 
+        classification_report = classification_report)
 
 
 @app.route('/dataprocess', methods=['GET', 'POST'])
 def dataprocess():
     return render_template("dataprocess.html")
+
+
+def preProcessData(chosendata, chosenimputation):
+    arrfurl = chosendata + ".arff"
+    rawDfList = main.getRawDfList(arrfurl);
+    if (chosenimputation != "null"):
+        rawDfList = preprocess.meanImputation(rawDfList)
+    rawDfList = preprocess.overSampleSmote(rawDfList)
+    # rawDfList[0] = rawDfList[0][rawDfList[0]['Attr1'] > 0.3]  # less value for dataframe
+    Df_missing_stats = main.getMissingStats(rawDfList)
+    return  rawDfList[0], Df_missing_stats
+
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("nodata.html")
 
 
 if __name__=="__main__":
--- process.py
+++ process.py
@@ -13,26 +13,10 @@ from sklearn.metrics import confusion_matrix
 
 def processData(df_list):
     print('processing data...')
-    decisionTree(df_list)
+    # decisionTree(df_list)
 
 
-def traintestsplit(df_list):
-    print('using train test split...')
-    df = df_list[0]
-
-
-
-
-def decisionTree(df_list):
-    print('using decision tree...')
-    labels = ['net profit / total assets', 'Liabilities / total assets', 'working capital / total assets', 'current assets / short-term liabilities', '[(cash + short-term securities + receivables - short-term liabilities) / (operating expenses - depreciation)] * 365', 'retained earnings / total assets', 'EBIT / total assets', 'book value of equity / total liabilities', 'sales / total assets', 'equity / total assets', '(gross profit + extraordinary items + financial expenses) / total assets', 'gross profit / short-term liabilities', '(gross profit + depreciation) / sales', '(gross profit + interest) / total assets', '(total liabilities * 365) / (gross profit + depreciation)', '(gross profit + depreciation) / total liabilities', 'total assets / total liabilities', 'gross profit / total assets', 'gross profit / sales', '(inventory * 365) / sales', 'sales (n) / sales (n-1)', 'sales (n) / sales (n-1)', 'net profit / sales', 'gross profit ( in 3 years) / total assets', '(equity - share capital) / total assets', '(net profit + depreciation) / total liabilities', 'profit on operating activities / financial expenses', 'working capital / fixed assets', 'logarithm of total assets', '(total liabilities - cash) / sales', '(gross profit + interest) / sales', '(current liabilities * 365) / cost of products sold', 'operating expenses / short-term liabilities', 'operating expenses / total liabilities', 'profit on sales / total assets', 'total sales / total assets', '(current assets - inventories) / long-term liabilities', 'constant capital / total assets', 'profit on sales / sales', '(current assets - inventory - receivables) / short-term liabilities', 'total liabilities / ((profit on operating activities + depreciation) * (12/365))', 'profit on operating activities / sales', 'rotation receivables + inventory turnover in days', '(receivables * 365) / sales', 'net profit / inventory', '(current assets - inventory) / short-term liabilities', '(inventory * 365) / cost of products sold', 'EBITDA (profit on operating activities - depreciation) / total assets', 'EBITDA (profit on operating activities - depreciation) / sales', 'current assets / total liabilities', 'short-term liabilities / total assets', '(short-term liabilities * 365) / cost of products sold)', 'equity / fixed assets', 'constant capital / fixed assets', 'working capital', '(sales - cost of products sold) / sales', '(current assets - inventory - short-term liabilities) / (sales - gross profit - depreciation)', 'total costs / total sales', 'long-term liabilities / equity', 'sales / inventory', 'sales / receivables', '(short-term liabilities * 365) / sales', 'sales / short-term liabilities', 'sales / fixed assets']
-    df = df_list[0]
-    features = list(df.columns[:64])
-    
-    X = df[features]
-    y = df[64]
-    
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20) # train/test split
+def kfold(df, k):
     kf = KFold(n_splits=2)
     kf.get_n_splits(X)
     print(kf) 
@@ -44,6 +28,36 @@ def decisionTree(df_list):
         y_train, y_test = y[train_index], y[test_index]
 
 
+def traintestsplit(df, test_size):
+    print('using train test split...')
+    # labels = ['net profit / total assets', 'Liabilities / total assets', 'working capital / total assets', 'current assets / short-term liabilities', '[(cash + short-term securities + receivables - short-term liabilities) / (operating expenses - depreciation)] * 365', 'retained earnings / total assets', 'EBIT / total assets', 'book value of equity / total liabilities', 'sales / total assets', 'equity / total assets', '(gross profit + extraordinary items + financial expenses) / total assets', 'gross profit / short-term liabilities', '(gross profit + depreciation) / sales', '(gross profit + interest) / total assets', '(total liabilities * 365) / (gross profit + depreciation)', '(gross profit + depreciation) / total liabilities', 'total assets / total liabilities', 'gross profit / total assets', 'gross profit / sales', '(inventory * 365) / sales', 'sales (n) / sales (n-1)', 'sales (n) / sales (n-1)', 'net profit / sales', 'gross profit ( in 3 years) / total assets', '(equity - share capital) / total assets', '(net profit + depreciation) / total liabilities', 'profit on operating activities / financial expenses', 'working capital / fixed assets', 'logarithm of total assets', '(total liabilities - cash) / sales', '(gross profit + interest) / sales', '(current liabilities * 365) / cost of products sold', 'operating expenses / short-term liabilities', 'operating expenses / total liabilities', 'profit on sales / total assets', 'total sales / total assets', '(current assets - inventories) / long-term liabilities', 'constant capital / total assets', 'profit on sales / sales', '(current assets - inventory - receivables) / short-term liabilities', 'total liabilities / ((profit on operating activities + depreciation) * (12/365))', 'profit on operating activities / sales', 'rotation receivables + inventory turnover in days', '(receivables * 365) / sales', 'net profit / inventory', '(current assets - inventory) / short-term liabilities', '(inventory * 365) / cost of products sold', 'EBITDA (profit on operating activities - depreciation) / total assets', 'EBITDA (profit on operating activities - depreciation) / sales', 'current assets / total liabilities', 'short-term liabilities / total assets', '(short-term liabilities * 365) / cost of products sold)', 'equity / fixed assets', 'constant capital / fixed assets', 'working capital', '(sales - cost of products sold) / sales', '(current assets - inventory - short-term liabilities) / (sales - gross profit - depreciation)', 'total costs / total sales', 'long-term liabilities / equity', 'sales / inventory', 'sales / receivables', '(short-term liabilities * 365) / sales', 'sales / short-term liabilities', 'sales / fixed assets']
+    # df = df_list[0]
+    features = list(df.columns[:64])
+    X = df[features]
+    y = df[64]
+    
+    print(y)
+    if (test_size == "null"):
+        test_size = 0.2
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+    return  X_train, X_test, y_train, y_test
+
+
+
+def decisionTree(X_train, X_test, y_train, y_test):
+    print('using decision tree...')
+    # labels = ['net profit / total assets', 'Liabilities / total assets', 'working capital / total assets', 'current assets / short-term liabilities', '[(cash + short-term securities + receivables - short-term liabilities) / (operating expenses - depreciation)] * 365', 'retained earnings / total assets', 'EBIT / total assets', 'book value of equity / total liabilities', 'sales / total assets', 'equity / total assets', '(gross profit + extraordinary items + financial expenses) / total assets', 'gross profit / short-term liabilities', '(gross profit + depreciation) / sales', '(gross profit + interest) / total assets', '(total liabilities * 365) / (gross profit + depreciation)', '(gross profit + depreciation) / total liabilities', 'total assets / total liabilities', 'gross profit / total assets', 'gross profit / sales', '(inventory * 365) / sales', 'sales (n) / sales (n-1)', 'sales (n) / sales (n-1)', 'net profit / sales', 'gross profit ( in 3 years) / total assets', '(equity - share capital) / total assets', '(net profit + depreciation) / total liabilities', 'profit on operating activities / financial expenses', 'working capital / fixed assets', 'logarithm of total assets', '(total liabilities - cash) / sales', '(gross profit + interest) / sales', '(current liabilities * 365) / cost of products sold', 'operating expenses / short-term liabilities', 'operating expenses / total liabilities', 'profit on sales / total assets', 'total sales / total assets', '(current assets - inventories) / long-term liabilities', 'constant capital / total assets', 'profit on sales / sales', '(current assets - inventory - receivables) / short-term liabilities', 'total liabilities / ((profit on operating activities + depreciation) * (12/365))', 'profit on operating activities / sales', 'rotation receivables + inventory turnover in days', '(receivables * 365) / sales', 'net profit / inventory', '(current assets - inventory) / short-term liabilities', '(inventory * 365) / cost of products sold', 'EBITDA (profit on operating activities - depreciation) / total assets', 'EBITDA (profit on operating activities - depreciation) / sales', 'current assets / total liabilities', 'short-term liabilities / total assets', '(short-term liabilities * 365) / cost of products sold)', 'equity / fixed assets', 'constant capital / fixed assets', 'working capital', '(sales - cost of products sold) / sales', '(current assets - inventory - short-term liabilities) / (sales - gross profit - depreciation)', 'total costs / total sales', 'long-term liabilities / equity', 'sales / inventory', 'sales / receivables', '(short-term liabilities * 365) / sales', 'sales / short-term liabilities', 'sales / fixed assets']
+    # df = df_list[0]
+    # features = list(df.columns[:64])
+    
+    # X = df[features]
+    # y = df[64]
+    
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20) # train/test split
+    
+
+    # X_train, X_test, y_train, y_test = traintestsplit(df)
+
     dt = DecisionTreeClassifier(min_samples_split=20, random_state=99)
     dt.fit(X_train, y_train)
 
@@ -52,16 +66,17 @@ def decisionTree(df_list):
 
     y_pred = dt.predict(X_test)
 
-    cal_accuracy(y_test, y_pred)
+    return cal_accuracy(y_test, y_pred)
     
 
-def cal_accuracy(y_test, y_pred): 
-    print("Confusion Matrix: ", confusion_matrix(y_test, y_pred)) 
-    
-    print ("Accuracy : ", accuracy_score(y_test,y_pred)*100) 
-
-    print("Report : ", classification_report(y_test, y_pred)) 
-
+def cal_accuracy(y_test, y_pred):
+    # print("Confusion Matrix: ", confusion_matrix(y_test, y_pred))
+    confusion_matrix_result = confusion_matrix(y_test, y_pred)
+    # print ("Accuracy : ", accuracy_score(y_test,y_pred)*100) 
+    accuracy_score_result = accuracy_score(y_test,y_pred)*100
+    print("Report : ", classification_report(y_test, y_pred))
+    classification_report_result = classification_report(y_test, y_pred, output_dict=True)
+    return confusion_matrix_result, accuracy_score_result, classification_report_result
 
 def createDecisionTreeGraph(dt, labels):
     dot_data = export_graphviz(dt, feature_names=labels,filled=True, rounded=True, special_characters=True)
